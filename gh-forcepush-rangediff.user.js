35610993883659299130aefb0581c353c5536431
Daniel Miller <millerdev@gmail.com>
2026-09-17 08:47:09 -0400

    Add full command to tooltip

--- gh-forcepush-rangediff.user.js
+++ gh-forcepush-rangediff.user.js
@@ -48,7 +48,7 @@
     const button = document.createElement('button');
     button.type = 'button';
     button.className = 'Button--invisible Button--small Button Button--invisible-noVisuals float-right ml-2';
-    button.title = 'Copy git range-diff command';
+    button.title = 'Copy git range-diff command: ' + cmd;
     button.setAttribute('aria-label', 'Copy git range-diff command');
     button.innerHTML = `<span class="Button-content"><span class="Button-label">${COPY_ICON}</span></span>`;
 
